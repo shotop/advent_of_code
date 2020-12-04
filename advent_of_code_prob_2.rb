b7dec7f72355fe528e71dd9ee86ad24fb1967009
Samuel Hotop <shotop@Samuels-MacBook-Pro.local>
2020-12-03 22:09:12 -0600

    problem 2 day 2

--- advent_of_code_prob_2.rb
+++ advent_of_code_prob_2.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 input = File.readlines("/Users/shotop/Coding/Advent_Of_Code_2020/advent_of_code_2.txt", chomp: true)
 
+#prob 1
 res = input.map(&:split).select do |policy|
   count_range = policy.first.split("-").map(&:to_i)
   char = policy[1].delete(":")
@@ -11,3 +12,23 @@ res = input.map(&:split).select do |policy|
 end
 
 puts res.count
+
+
+#prob 2
+res = input.map(&:split).select do |policy|
+  indexes = policy.first.split("-").map(&:to_i)
+  char = policy[1].delete(":")
+  password = policy.last
+
+  next unless password.include?(char)
+
+  char_at_pos = []
+  char_at_pos.append(password[indexes.first - 1] == char)
+  char_at_pos.append(password[indexes.last - 1] == char)
+
+  char_at_pos.count(true) == 1
+end
+
+puts res.count
+
+
