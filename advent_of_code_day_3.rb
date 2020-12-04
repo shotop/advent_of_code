ce9721dc57ade7bf7dfe5d521723d61e1e519e68
Samuel Hotop <shotop@Samuels-MacBook-Pro.local>
2020-12-04 10:28:41 -0600

    broken part two

--- advent_of_code_day_3.rb
+++ advent_of_code_day_3.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 input = File.readlines("/Users/shotop/Coding/Advent_Of_Code_2020/day_3_input.txt", chomp: true)
 
+#part 1
 index = 0
 trees = 0
 
@@ -11,7 +12,28 @@ input.each do |line|
   end
 
   trees += 1 if line[index] == "#"
-  index += 3
+  index += 1
 end
 
 p trees
+
+
+#part 2
+slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+
+slopes.each do |slope|
+  index = 0
+  trees = 0
+
+  input.each_slice(slope.last) do |slice|
+    line = slice.first
+
+    while index > line.length do
+      line = line.concat(line)
+    end
+
+    trees += 1 if line[index] == "#"
+    index += slope.first
+  end
+  p trees
+end
