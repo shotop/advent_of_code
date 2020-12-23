94012cf200c07490d21f25badc236f7342852a45
Samuel Hotop <shotop@Samuels-MacBook-Pro.local>
2020-12-23 14:41:40 -0600

    day 8 part 2

--- advent_of_code_day_8.rb
+++ advent_of_code_day_8.rb
@@ -7,31 +7,85 @@ require 'pry'
   inst.split.append(false)
 end
 
-index = 0
-acc = 0
+# index = 0
+# acc = 0
 
-while index < @instructions.length
-  inst = @instructions[index]
-  bool = inst.last
-  operation = inst.first
-  arg = inst[1]
+# while index < @instructions.length
+#   inst = @instructions[index]
+#   bool = inst.last
+#   operation = inst.first
+#   arg = inst[1]
 
-  if bool
-    puts acc
-    break
+#   if bool
+#     puts acc
+#     break
+#   end
+
+#   case operation
+
+#   when "nop"
+#     inst.replace([operation, arg, true])
+#     index = index + 1
+#   when "acc"
+#     inst.replace([operation, arg, true])
+#     acc += arg.to_i
+#     index = index + 1
+#   when "jmp"
+#     inst.replace([operation, arg, true])
+#     index = index + arg.to_i
+#   end
+# end
+
+#prob 2
+def follow_instructions(instructions)
+  index = 0
+  acc = 0
+
+  while index < instructions.length
+    inst = instructions[index]
+    bool = inst.last
+    operation = inst.first
+    arg = inst[1]
+
+    if bool
+      raise StandardError, "Infinite Loop Detected"
+    else
+      p acc
+    end
+
+
+    case operation
+
+    when "nop"
+      inst.replace([operation, arg, true])
+      index = index + 1
+    when "acc"
+      inst.replace([operation, arg, true])
+      acc += arg.to_i
+      index = index + 1
+    when "jmp"
+      inst.replace([operation, arg, true])
+      index = index + arg.to_i
+    end
+  end
+end
+
+
+@instructions.each_with_index do |instruction, main_index|
+  internal_list = @instructions.map(&:clone)
+
+  if instruction.first == "jmp"
+    internal_list[main_index].shift
+    internal_list[main_index].unshift("nop")
+  elsif internal_list[main_index].first == "nop"
+    internal_list[main_index].shift
+    internal_list[main_index].unshift("jmp")
   end
 
-  case operation
-  when "nop"
-    inst.replace([operation, arg, true])
-    index = index + 1
-  when "acc"
-    inst.replace([operation, arg, true])
-    acc += arg.to_i
-    index = index + 1
-  when "jmp"
-    inst.replace([operation, arg, true])
-    index = index + arg.to_i
+  begin
+    follow_instructions(internal_list)
+  rescue StandardError => error
+    next
   end
 end
 
