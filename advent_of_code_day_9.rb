7731d03bf1d3e455a884371f1be770e88b2295db
shotop <samuel.j.hotop@gmail.com>
2020-12-28 22:00:07 -0600

    day 9 part 2

--- advent_of_code_day_9.rb
+++ advent_of_code_day_9.rb
@@ -3,12 +3,24 @@ require 'pry'
 #prob 1
 @input = File.readlines("/Users/shotop/Coding/Advent_Of_Code_2020/day_9_input.txt", chomp: true).map(&:to_i)
 
+# @input.each_with_index do |number, index|
+#   next unless index >= 25
+
+#   preamble = @input.slice(index - 25, 25)
+
+#   if preamble.combination(2).detect { |a, b| a + b == number }.nil?
+#     puts number
+#   end
+# end
+
+#prob 2
+sum = 393911906
+
 @input.each_with_index do |number, index|
-  next unless index >= 25
-
-  preamble = @input.slice(index - 25, 25)
-
-  if preamble.combination(2).detect { |a, b| a + b == number }.nil?
-    puts number
+  index.upto(@input.length) do |inner_index|
+    if @input[index..inner_index].inject(:+) == sum
+      set = @input[index..inner_index].sort
+      puts set.first + set.last
+    end
   end
 end
